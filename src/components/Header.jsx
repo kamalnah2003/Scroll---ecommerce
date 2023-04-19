2b3d5957c9b30b03d2d108c90a8dbc0118d684d5
kamalnah2003 <54796340+kamalnah2003@users.noreply.github.com>
2023-04-19 18:52:37 +0530

    1 st video complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,41 @@
 import React from 'react'
+import { useLocation,useNavigate } from 'react-router-dom'
+
 
 export default function Header() {
-  return (
-    <div className="bg-black border-b-2">
+    const navigate = useNavigate()
+
+ 
+
+ 
+    const location =useLocation()
+    console.log(location.pathname);
+    function pathMathRoute(route){
+        if(route === location.pathname){
+            return true
+        }
+
+    }  
+  
+    return (
+    <div className="bg-white border shadow-2xl sticky top-0">
         <header className='flex justify-between items-center px-3 max-w-6xl mx-auto '>
-            <div className="bg-slate-800">
+            <div className="shadow-lg stroke-black	">
                 <img src='https://github.com/kamalnah2003/Scroll---ecommerce/blob/main/src/components/logo.png?raw=true' 
-                alt='logo' className="h-12 cursor-pointer"/>
+                alt='logo' className="h-12 cursor-pointer" onClick={()=>navigate("/")}/>
                 
             </div>
             <div>
                 <ul className='flex space-x-10'>
-                    <li>Home</li>
-                    <li>Offers</li>
-                    <li>Sign in</li>
-                    <li>d</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/") && "text-black border-b-red-500"
+              }`} onClick={()=>navigate("/")}>Home</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/offers") && "text-black border-b-red-500"
+              }`}onClick={()=>navigate("/offers")}>Offers</li>
+                    <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/signin") && "text-black border-b-red-500"
+              }`}onClick={()=>navigate("/signin")}>Sign in</li>
                 </ul>
             </div>
         </header>

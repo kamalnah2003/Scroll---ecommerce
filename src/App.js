1974f088ef0f73fe51efae86b9ab3b7d7c54d7b1
kamalnah2003 <54796340+kamalnah2003@users.noreply.github.com>
2023-04-19 16:48:28 +0530

    hi

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Profile from "./pages/Profile";
 import Forgotpassword from "./pages/Forgotpassword";
 import Offers from "./pages/Offers";
 import Signin from "./pages/Signin";
+import Header from "./components/Header";
 
 
 function App() {
   return (
    <Router>
+    <Header/>
     <Routes>
       <Route path="/" element = {<Home/>}/>
       <Route path="/sign-in" element ={<Signin/>}/>

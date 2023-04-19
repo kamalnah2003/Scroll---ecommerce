6af1fd413d96a36ac75ffc236b653f1344478b07
kamalnah2003 <54796340+kamalnah2003@users.noreply.github.com>
2023-04-19 14:31:16 +0530

    installedd react,tail

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+function App() {
+  return (
+    <>
+    <h1 className='text-xl bg-red-300'>
+      hiiiiiiiiiii
+    </h1>
+    
+    </>
+  );
+}
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  );
+}
+
+export default App;

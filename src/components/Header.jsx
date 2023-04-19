fcff55f0e077e00e5cd5a6d3458e719183683391
kamalnah2003 <54796340+kamalnah2003@users.noreply.github.com>
2023-04-19 17:55:49 +0530

    s

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>
-        <header>
-            <div className="bg-slate-700">
+    <div className="bg-black border-b-2">
+        <header className='flex justify-between items-center px-3 max-w-6xl mx-auto '>
+            <div className="bg-slate-800">
                 <img src='https://github.com/kamalnah2003/Scroll---ecommerce/blob/main/src/components/logo.png?raw=true' 
-                alt='logo' className="h-12 "/>
+                alt='logo' className="h-12 cursor-pointer"/>
                 
+            </div>
+            <div>
+                <ul className='flex space-x-10'>
+                    <li>Home</li>
+                    <li>Offers</li>
+                    <li>Sign in</li>
+                    <li>d</li>
+                </ul>
             </div>
         </header>
     </div>
